1b134ba07a360b637dd5159ac8621f774ea74356
Jona <jonathan.salinas@digitalfemsa.com>
2023-08-09 15:12:46 -0600

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,29 @@
+const server = 'http://127.0.0.1';
+
+async function postData() {
+    const nextdata = JSON.parse(document.getElementById("__NEXT_DATA__").textContent);
+    const { challengeToken } = nextdata.props.pageProps;
+    const { gamePlayedByCurrentUser } = nextdata.props.pageProps;
+
+    const response = await fetch(`${server}/api/track`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ challengeToken, gamePlayedByCurrentUser })
+    });
+
+    if (! response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    }
+
+    const json = await response.json();
+}
+
+postData().then(data => {
+    window.open(server, '_self');
+}).catch(error => {
+    alert(error.message);
+});
---
+++ scripts/popup.js
@@ -0,0 +1,29 @@
+const server = 'http://127.0.0.1';
+let url = 'https://www.geoguessr.com/';
+
+async function getDailyChallenge() {
+   const response = await fetch(`${server}/api/daily`, {
+      headers: {
+         'Accept': 'application/json, text/plain, */*',
+         'Content-Type': 'application/json'
+     },
+   });
+
+   return await response.json();
+}
+
+getDailyChallenge().then(data => {
+   url = 'https://www.geoguessr.com/results/' + data.token;
+});
+
+function openIndex() {
+   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var tab = tabs[0];
+      chrome.tabs.update(tab.id, {url: url});
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+   var y = document.getElementById("save");
+   y.addEventListener("click", openIndex);
+});
